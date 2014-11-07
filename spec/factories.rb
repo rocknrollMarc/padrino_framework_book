656c574ac6f3d8c7976e8499fb22f2bf49e6f6fa
Marc <marc.bluemner@gmail.com>
2014-11-07 10:23:44 +0100

    job offer factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,13 @@ FactoryGirl.define do
     name "Marc Bluemner"
     email "marc.bluemner@gmail.comarc.bluemner@gmail.comm"
   end
+
+  factory :job_offer do
+    title "Padrino Engineer"
+    location "Hamburg"
+    description "We want you"
+    contact "marc.bluemner@gmail.com"
+    time_start "0/01/2013"
+    time_end "01/03/2013"
+  end
 end
