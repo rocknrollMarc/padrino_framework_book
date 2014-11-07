30282150e760a9626d870e0fc6dae2af1a818e59
Marc <marc.bluemner@gmail.com>
2014-11-07 10:18:47 +0100

    require fatories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 RACK_ENV = 'test' unless defined?(RACK_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 Dir[File.expand_path(File.dirname(__FILE__) + "/../app/helpers/**/*.rb")].each(&method(:require))
+require File.dirname(__FILE__) + "/factories"
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
